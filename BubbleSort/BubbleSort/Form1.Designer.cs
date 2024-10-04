050810abc91115c6228164e47997dfebc7ca123d
cfbj11 <cfbriones@uamv.edu.ni>
2024-10-04 00:26:18 -0600

    Orden Ascendente

---
+++ BubbleSort/BubbleSort/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BubbleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbNumAsc = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbNumDes = new System.Windows.Forms.ListBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lbNum = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numeros:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descendiente";
+            // 
+            // lbNumAsc
+            // 
+            this.lbNumAsc.FormattingEnabled = true;
+            this.lbNumAsc.ItemHeight = 16;
+            this.lbNumAsc.Location = new System.Drawing.Point(86, 28);
+            this.lbNumAsc.Name = "lbNumAsc";
+            this.lbNumAsc.Size = new System.Drawing.Size(76, 116);
+            this.lbNumAsc.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ascendente";
+            // 
+            // lbNumDes
+            // 
+            this.lbNumDes.FormattingEnabled = true;
+            this.lbNumDes.ItemHeight = 16;
+            this.lbNumDes.Location = new System.Drawing.Point(168, 28);
+            this.lbNumDes.Name = "lbNumDes";
+            this.lbNumDes.Size = new System.Drawing.Size(91, 116);
+            this.lbNumDes.TabIndex = 5;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(15, 150);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(244, 55);
+            this.btnMostrar.TabIndex = 6;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lbNum
+            // 
+            this.lbNum.FormattingEnabled = true;
+            this.lbNum.ItemHeight = 16;
+            this.lbNum.Location = new System.Drawing.Point(15, 28);
+            this.lbNum.Name = "lbNum";
+            this.lbNum.Size = new System.Drawing.Size(62, 116);
+            this.lbNum.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.lbNum);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lbNumDes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbNumAsc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbNumAsc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbNumDes;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lbNum;
+    }
+}
+
---
+++ BubbleSort/BubbleSort/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSort
+{
+    public partial class Form1 : Form
+    {
+        int[] nums = new int[7] {1,4,6,8,0,9,2};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                lbNum.Items.Add(nums[i]);
+                NumAsc();
+                lbNumAsc.Items.Add(nums[i]);
+                //NumDes();
+                //lbNumDes.Items.Add(nums[i]);
+            }
+        }
+        private void NumAsc()
+        {
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 0; j < nums.Length - i - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])  //  >
+                    {
+                        temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        /*
+        private void NumDes()
+        {
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 0; j < nums.Length - i - 1; j++)
+                {
+                    if (nums[j] < nums[j + 1])  //  <
+                    {
+                        temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+        }*/
+    }
+}
